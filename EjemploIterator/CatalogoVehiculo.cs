24bbbb0fcc65522576d7212b028147439f27271b
iza02 <iza02@DESKTOP-2N59KCF>
2023-12-04 15:01:14 -0700

    Agregar archivos de proyecto.

---
+++ EjemploIterator/CatalogoVehiculo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploIterator
+{
+    public class CatalogoVehiculo : Catalogo<Vehiculo,
+  IteradorVehiculo>
+    {
+
+        public CatalogoVehiculo()
+        {
+            contenido.Add(new Vehiculo("vehiculo economico"));
+            contenido.Add(new Vehiculo("pequeño vehiculo economico"));
+            contenido.Add(new Vehiculo("vehoculo de gran calidad"));
+        }
+    }
+
+}
